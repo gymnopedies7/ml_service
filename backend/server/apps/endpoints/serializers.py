c300c0be23da1910d1349bb2c475e6b90e550af5
Hwang <david@hwangminsuui-MacBook-Pro.local>
2020-08-29 11:08:33 +0900

    Serializer 작성완료

---
+++ backend/server/apps/endpoints/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from apps.endpoints.models import *
+
+class EndpointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Endpoint
+        read_only_fields = ("id", "name", "owner", "created_at")
+        fields = read_only_fields
+
+class MLAlgorithmSerializer(serializers.ModelSerializer):
+    current_status = serializers.SerializerMEthodField(read_only = True)
+
+    def get_current_status(self, mlalgorithm):
+        return MLAlgorithmStatus.objects.filter(parent_mlalgorithm = mlalgorithm).latest('created_at').status
+    
+    class Meta:
+        model = MLAlgorithm
+        read_only_fields = ("id", "name", "description", "code", "version","owner","created_at",
+                            "parent_endpoint", "current_status")
+        fields = read_only_fields
+
+class MLAlgorithmsStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLAlgorithmStatus
+        read_only_fields = ("id", "active")
+        fields = ("id", "active", "status", "created_by", "created_at", "parent_mlalgorithm")
+
+class MLRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLRequest
+        read_only_fields = ("id","input_data","full_response","response","created_at","parent_mlalgorithm")
+        fields = ("id", "input_data","full_response","response","feedback","created_at","parent_mlalgorithm")
